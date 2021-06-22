856686a3047373a0edd1afa6e026724e8112a449
MJNabor <mariajhazmine.nabor@gmail.com>
2021-06-22 16:21:13 +0800

    Updates

---
+++ precache-manifest.1dcd7bab92b122199704c4698fb39555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c605dc691b52db0364a",
+    "url": "/me/static/css/main.7d39ec8b.chunk.css"
+  },
+  {
+    "revision": "8c605dc691b52db0364a",
+    "url": "/me/static/js/main.8c605dc6.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "2fc43f90d1823baecaf2ca1ab3031b7a",
+    "url": "/me/index.html"
+  }
+];
